5d25332bd245b8e24a0d38eb9de4cc94dd00a6a9
arisprogramming <dossouaristide38@gmail.com>
2022-10-15 18:46:25 +0000

    complete

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '30gotmao3f';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-sh4pbqc4.us.auth0.com',
+  clientId: '9ZZdiOktc5az82bhEDEfytHUzdSvoKPt',
+  callbackUrl: 'http://localhost:3000/callback'
+};
